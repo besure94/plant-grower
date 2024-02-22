fa43134a25309d94efdcfdc4868937150b19df0b
Brian Scherner <brianscherner@gmail.com>
2024-02-22 15:18:52 -0800

    Remove tests and code after app refactor.

--- __tests__/plant.test.js
+++ __tests__/plant.test.js
@@ -1,7 +1,4 @@
 import Plant from '../src/js/plant.js';
-import { hydrate } from '../src/js/plant.js';
-import { feed } from '../src/js/plant.js';
-import { changePlantState } from '../src/js/plant.js';
 import { changeState } from '../src/js/plant.js';
 
 describe('Plant grower application tests', () => {
@@ -11,24 +8,6 @@ describe('Plant grower application tests', () => {
     expect(plant.water).toEqual(0);
     expect(plant.soil).toEqual(0);
     expect(plant.light).toEqual(0);
-  });
-
-  test('App should contain a method which adds 1 the plants "water" property.', () => {
-    const plant = new Plant();
-    const result = hydrate(plant);
-    expect(result.water).toEqual(1);
-  });
-
-  test('App should contain a method which adds 1 the plants "soil" property.', () => {
-    const plant = new Plant();
-    const result = feed(plant);
-    expect(result.soil).toEqual(1);
-  });
-
-  test('App should contain a method which can alter any of the plants properties by 1', () => {
-    const plant = new Plant();
-    const result = changePlantState(plant, "light");
-    expect(result.light).toEqual(1);
   });
 
   test('App should contain a method which can alter properties of any object.', () => {
--- src/js/plant.js
+++ src/js/plant.js
@@ -6,27 +6,6 @@ export default class Plant {
   }
 }
 
-export const hydrate = (plant) => {
-  return {
-    ...plant,
-    water: (plant.water || 0) + 1
-  }
-};
-
-export const feed = (plant) => {
-  return {
-    ...plant,
-    soil: (plant.soil || 0) + 1
-  }
-};
-
-export const changePlantState = (plant, property) => {
-  return {
-    ...plant,
-    [property]: (plant[property] || 0) + 1
-  }
-};
-
 export const changeState = (state, prop) => {
   return {
     ...state,

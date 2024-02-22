8fdd07053aba9a4e54b61f24cda4d2a567cf5dcc
Brian Scherner <brianscherner@gmail.com>
2024-02-22 14:51:11 -0800

    Write passing test to add to plant's 'water' property.

--- __tests__/plant.test.js
+++ __tests__/plant.test.js
@@ -1,16 +1,18 @@
 import Plant from '../src/js/plant.js';
+import { hydrate } from '../src/js/plant.js';
 
-describe('Plant grower', () => {
-
-  let plant;
-
-  beforeEach(() => {
-    plant = new Plant();
-  });
+describe('Plant grower application tests', () => {
 
   test('Should create a class called Plant that has properties for water, soil and light.', () => {
+    const plant = new Plant();
     expect(plant.water).toEqual(0);
     expect(plant.soil).toEqual(0);
     expect(plant.light).toEqual(0);
+  });
+
+  test('App should contain a method which adds to the plants "water" property.', () => {
+    const plant = new Plant();
+    const result = hydrate(plant);
+    expect(result.water).toEqual(1);
   });
 });
--- src/js/plant.js
+++ src/js/plant.js
@@ -6,30 +6,12 @@ export default class Plant {
   }
 }
 
-// const changeState = (prop) => {
-//   return (value) => {
-//     return (state) => ({
-//       ...state,
-//       [prop]: (state[prop] || 0) + value
-//     })
-//   }
-// };
+export const hydrate = (plant) => {
+  return {
+    ...plant,
+    water: (plant.water || 0) + 1
+  }
+};
 
-// const storeState = () => {
-//   let currentState = {};
-//   return (stateChangeFunction) => {
-//     const newState = stateChangeFunction(currentState);
-//     currentState = {...newState};
-//     return newState;
-//   }
-// }
-
-// const feed = changeState("soil");
-// const hydrate = changeState("water");
-// const giveLight = changeState("light");
-// const blueFood = changeState("soil")(5)
-// const greenFood = changeState("soil")(10)
-// const yuckyFood = changeState("soil")(-5)
-
-// const stateControl = storeState();
-// const fedPlant = stateControl(blueFood);
+const waterPlant = hydrate(Plant);
+waterPlant
